b74863ded3c441e3ee4690d5e7777fc5bb62c5de
Daniel Pires <danieldppires@gmail.com>
2021-02-12 21:35:03 -0300

    criamos a classe Page

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?php 
 
-require_once("vendor/autoload.php");
+	//O autoload vai chamar o vendor autoload no composer (as dependências). Sempre vai ter. Traz o que o projeto precisa
+	require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+	//Estes são namespaces. Eu tenho dezenas de classes dentro do vendor e aqui eu digo quais eu quero usar
+	use \Slim\Slim;
+	use \Hcode\Page;
 
-$app->config('debug', true);
+	$app = new Slim(); //Aqui é por causa das rotas. Antes chamava index.php, default.php, cadastro.php
+	//Agora por causa de SEO e rankeamento é tudo rota. Passa o nome na URL e o Slim manda para algum lugar
 
-$app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
+	$app->config('debug', true);
 
-	$results = $sql->select("SELECT * FROM tb_users");
+	$app->get('/', function() //Aqui é a rota que estamos chamando. Quando uma chamada via get (padrão) sem nenhum tipo de rota chamar, carrega isto aqui (executa esta função).
+	{
+		$page = new Page(); //Neste momento (new) ele chama o construct que irá adicinar o header na tela
 
-	echo json_encode($results);
+		$page->setTpl("index"); //Neste momento ele chama o arquivo index.html (o corpo da página)
 
-});
+		//Aqui ele acaba a execução e o PHP vai limpar a memória, chamando o método destruct, que irá incluir o footer
+	});
 
-$app->run();
+	$app->run(); //É quem faz tudo acima rodar
 
  ?>
---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,60 @@
+<?php
+
+	namespace Hcode;
+
+	use Rain\Tpl;
+
+	class Page
+	{
+		private $tpl;
+		private $options = [];
+		private $defaults = [
+			"data"=>[]
+		];
+
+		public function __construct($opts = array())
+		{
+			$this->options = array_merge($this->defaults, $opts); //Se passar informação no segundo parâmetro e ele der conflito com o defaults ele sobreescreve o defaults e usa o opts. O merge vai mesclar as duas paginas dentro deste atributo
+
+			$config = array(
+				"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/", //Pasta onde está o template
+				"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/", //
+				"debug"         => false
+		   	);
+
+			Tpl::configure($config);
+
+			$this->tpl = new Tpl;
+
+			//As variáveis vão vir de acordo com a rota no Slim
+			$this->setData($this->options["data"]);
+
+			$this->tpl->draw("header"); //Esse arquivo vai repetir em todas as páginas
+		}
+
+		private function setData($data = array())
+		{
+			foreach ($data as $key => $value)
+			{
+				$this->tpl->assign($key, $value); 
+			}
+		}
+
+		//Este é o HTML do conteúdo (corpo que será pagina de login, pagina de cadastro, etc)
+		//Recebe o nome do template, as variaveis e se retorna HTML ou já joga na tela
+		public function setTpl($name, $data = array(), $returnHTML = false)
+		{
+			$this->setData($data);
+			return $this->tpl->draw($name, $returnHTML);
+		}
+		
+
+		public function __destruct()
+		{
+			//Quando esta classe sair da memória do PHP, vamos adicionar o footer
+			//Aqui podemos colocar javascripts e tudo que quisermos que repita em todas as telas
+			$this->tpl->draw("footer");
+		}
+	}
+
+?>
